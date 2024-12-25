c01eaae667b756abb5c1c26c2b7956cd80240b84
Abu Sayeed <sam@Abus-MacBook-Air.local>
2024-12-25 13:27:20 -0500

    finished

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,64 @@
-let value1, value2, area;
-function getInput(input1, input2){
-    value1 = parseFloat(document.getElementById(input1).value);
-    value2 = parseFloat(document.getElementById(input2).value);
+
+function getInput(id){
+    const input = parseFloat(document.getElementById(id).value);
+    return input;
 }
 
-function setArea(areaHolder){
-    const areaContainer = document.getElementById(areaHolder);
-    areaContainer.innerText = area;
+function setArea(area, calculatedArea){
+    const areaSpan = document.getElementById(area);
+    areaSpan.innerText = calculatedArea;
 }
 
-function calculateArea(){
-    // const triangleBase = parseFloat(document.getElementById("triangle-base").value);
-    // const triangleHeight = parseFloat(document.getElementById("triangle-height").value);
-    getInput("triangle-base", "triangle-height");
+function calculateTriangle(){
+    const base = getInput('triangle-base');
+    const height = getInput('triangle-height');
 
-    area = .5 * value1 * value2;
+    const triangleArea = .5 * base * height;
 
-    // const triangleArea = document.getElementById("area-holder");
-    // triangleArea.innerText = area;
+    setArea("triangle-area-span", triangleArea);
+}
 
-    setArea("area-holder");
+function calculateRectangle(){
+    const width = getInput('rectangle-width');
+    const lenght = getInput('rectangle-length');
+
+    const rectangleArea = width * lenght;
+
+    setArea('rectangle-area-span', rectangleArea);
+}
+
+function calculateParallelogram(){
+    const base = getInput('parallelogram-base');
+    const height = getInput('parallelogram-height');
+
+    const parallelogramArea = base * height;
+
+    setArea('parallelogram-area-span', parallelogramArea);
+}
+
+function calculateRhombus(){
+    const d1 = getInput('rhombus-d1');
+    const d2 = getInput('rhombus-d2');
+    
+    const rhombusArea = .5 * d1 * d2;
+
+    setArea('rhombus-area-span', rhombusArea);
+}
+
+function calculatePentagon(){
+    const perimeter = getInput('pentagon-perimeter');
+    const apothem = getInput('pentagon-apothem');
+    
+    const pentagonArea = .5 * perimeter * apothem;
+
+    setArea('pentagon-area-span', pentagonArea);
+}
+
+function calculateEllipse(){
+    const a = getInput('ellipse-a');
+    const b = getInput('ellipse-b');
+    
+    const ellipseArea = (Math.PI * a * b).toFixed(2);
+
+    setArea('ellipse-area-span', ellipseArea);
 }
